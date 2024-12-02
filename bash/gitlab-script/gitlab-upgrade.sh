ae6b30a1d5748a8816108774d0ed94f8899c085e
somaz <genius5711@gmail.com>
2024-12-02 15:17:56 +0900

    fix: bash/gitlab-script

--- bash/gitlab-script/gitlab-upgrade.sh
+++ bash/gitlab-script/gitlab-upgrade.sh
@@ -2,6 +2,14 @@
 
 version=16.3.4-ce.0  # write your update version
 
+# # 백업 생성 (선택적)
+# echo "Creating backup before upgrade..."
+# sudo gitlab-rake gitlab:backup:create
+
+# 현재 버전 확인
+echo "Current GitLab version"
+sudo gitlab-rake gitlab:env:info
+
 echo "Upgrading to GitLab $version"
 sudo apt update
 sudo apt-get install -y gitlab-ce=$version
@@ -10,5 +18,13 @@ sudo gitlab-ctl reconfigure
 sleep 10
 sudo gitlab-ctl restart
 sleep 10
+
+# 업그레이드 후 상태 확인
+echo "Checking GitLab status..."
+sudo gitlab-ctl status
+
+# 업그레이드 후 버전 확인
+echo "New GitLab version"
+sudo gitlab-rake gitlab:env:info
 
 echo "Upgrade completed"
